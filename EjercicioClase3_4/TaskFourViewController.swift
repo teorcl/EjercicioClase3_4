374121850e54d380814991fa5ad877402e97ae85
Teodoro <teo.calle.lara@gmail.com>
2022-11-08 02:20:15 -0500

    Se agrega cambio de imagen con cuando el numero es par o impar

--- EjercicioClase3_4/TaskFourViewController.swift
+++ EjercicioClase3_4/TaskFourViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class TaskFourViewController: UIViewController {
 
+    struct Const{
+        static let initialNumber = 1
+        static let finalNumber = 10
+        static let winnerNumber = 7
+    }
+    
+    @IBOutlet weak var randomNumberLabel: UILabel!
+    @IBOutlet weak var faceImageView: UIImageView!
+    
+    var randomNumber = Const.initialNumber
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
+    
+    
+    @IBAction func nextButtonPressed(_ sender: Any) {
+        play()
+    }
+    
+    func showNumberRandom(){
+        randomNumberLabel.text = "\(randomNumber)"
+    }
+    
+    func getRandomNumer(){
+        randomNumber = Int.random(in: Const.initialNumber...Const.finalNumber)
+    }
+    
+    func selectImage(){
+        //selecionar la imagen dependediendo de randomNumber
+        if randomNumber % 2 == 0 {
+            faceImageView.image = UIImage(named: "caraTriste")
+        }else{
+            faceImageView.image = UIImage(named: "caraFeliz")
+        }
+        
+    }
+    
+    
+    func play(){
+        getRandomNumer()
+        selectImage()
+        showNumberRandom()
+    }
 
 }
 
