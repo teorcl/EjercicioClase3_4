b7d14eec7cdcedb2cc1a19d617ce402cd7813301
Teodoro <teo.calle.lara@gmail.com>
2022-11-08 01:29:12 -0500

    Refactor name VIewCOntroller

--- EjercicioClase3_4/TaskFourViewController.swift
+++ EjercicioClase3_4/TaskFourViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TaskFourViewController.swift
 //  EjercicioClase3_4
 //
 //  Created by TEO on 8/11/22.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TaskFourViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
